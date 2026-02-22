820271eeadaa03934d25255349332a31eabd09bc
christmeya156-rgb <christmeya156@gmail.com>
2026-02-22 20:17:02 +0100

    Update Main.h

--- Main.h
+++ Main.h
@@ -1,1 +1,10 @@
 
+void menu(void);
+float division(float, float) ;
+float multiplication(float, float) ;
+float addition(float, float) ;
+float soustraction(float, float) ;
+float modulo(float, float) ;
+float racine(float, float) ;
+float puissance(float, float) ;
+float factorielle(float) ;
